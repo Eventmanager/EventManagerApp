2dd935bcba625a6b442a456511a1878dfc77125d
Seph <bob.vankamp@gmail.com>
2014-05-26 15:54:26 +0200

    Updated map code
    Gebruikt nu een builder pattern om afbeeldingen en vormen op de kaart te
    krijgen.

--- src/com/project/eventmanagerapp/Activity_Map.java
+++ src/com/project/eventmanagerapp/Activity_Map.java
@@ -46,49 +46,108 @@ public class Activity_Map extends FragmentActivity {
     	eventMap.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(51.9167, 4.5000), 12.0f));
     	
     	//Example code for shapes. In this case a triangle near the marker.
-    	createShape(new float[][]{{51.9167f,4.5000f},{51.9177f,4.5006f},{51.9157f,4.5090f}});
-    	createShape(new float[][]{{51.9177f,4.5000f},{51.9187f,4.5006f},{51.9167f,4.5090f}},Color.BLUE,Color.RED,5.0f);
+    	Polygon poly1 = new ShapeBuilder().points(new float[][]{{51.9167f,4.5000f},{51.9177f,4.5006f},{51.9157f,4.5090f}}).build();
+    	Polygon poly2 = new ShapeBuilder().points(new float[][]{{51.9187f,4.5000f},{51.9197f,4.5006f},{51.9177f,4.5090f}})
+    			.width(5f).fill(Color.RED).stroke(Color.BLUE).build();
     	
     	//Example code for an image. In this case near the marker.
-    	createImage(new LatLng(51.9167, 4.4980),2000f,"house",0f);
+    	GroundOverlay image1 = new ImageBuilder().position(new LatLng(51.9167, 4.4980)).width(2000f).image("house").build();
     }
     
-    /**
-     * Creates a polygon shape with a set colour as fill and/or stroke and a set of points as floats.
-     * If you don't supply a fill and stroke, it will default to black for stroke and transparent for fill.
-     */
-    private Polygon createShape(float[][] points){
-    	return createShape(points,null,null,null);
-    }
-    private Polygon createShape(float[][] points, Integer stroke, Integer fill,Float size) {
-    	
-    	PolygonOptions poly = new PolygonOptions();
-    	if(stroke != null)
-    		poly.strokeColor(stroke);
-    	if(fill != null)
-    		poly.fillColor(fill);
-    	if(size != null)
-    		poly.strokeWidth(size);
-    	
-    	for(float[] point : points)
-    		poly.add(new LatLng(point[0],point[1]));
-    	
-    	return eventMap.addPolygon(poly);
-    }
     
     /**
-     * Creates an image on the map, requires a position (latlng object), width and the image as a string. Width and height in meters.
+     * Creates an image on the map, requires a position (latlng object), width and the image as a string. Width in meters.
      * You can also supply a rotation, a float value describing the amount of degrees, clockwise.
      */
-    private GroundOverlay createImage(LatLng pos,float width,String im,float rotation){
+    private class ImageBuilder{
     	
-    	GroundOverlayOptions image = new GroundOverlayOptions()
-        .image(BitmapDescriptorFactory.fromResource(getResources().getIdentifier(im, "drawable", getPackageName())))
-        .position(pos, width);
-    	if(rotation != 0f)
-        image.bearing(rotation);
+    	LatLng position;
+    	Float width = null;
+    	Float rotation = null;
+    	String image = null;
     	
-    	return eventMap.addGroundOverlay(image);
+    	public ImageBuilder(){}
+    	
+    	public ImageBuilder position(LatLng pos){
+    		this.position = pos;
+    		return this;
+    	}
+    	
+    	public ImageBuilder width(Float w){
+    		this.width = w;
+    		return this;
+    	}
+    	
+    	public ImageBuilder rotation(Float rotation){
+    		this.rotation = rotation;
+    		return this;
+    	}
+    	
+    	public ImageBuilder image(String image){
+    		this.image= image;
+    		return this;
+    	}
+    	
+    	public GroundOverlay build(){
+    		GroundOverlayOptions im = new GroundOverlayOptions();
+    		
+    		if(this.image != null)
+    			im.image(BitmapDescriptorFactory.fromResource(getResources().getIdentifier(this.image, "drawable", getPackageName())));
+            
+    		if(this.position != null && this.width != null)
+            	im.position(this.position, this.width);
+        	
+    		if(rotation != null)
+        		im.bearing(this.rotation);
+        	
+        	return eventMap.addGroundOverlay(im);
+    	}
+    }
+    
+    /**
+     * Class to create a polygon shape with a set colour as fill and/or stroke and a set of points as floats.
+     */
+    private class ShapeBuilder{
+    	
+
+    	Float width = null;
+    	Integer fill = null;
+    	Integer stroke = null;
+    	float[][] points;
+    	
+    	public ShapeBuilder(){}
+    	
+    	public ShapeBuilder stroke(Integer stroke){
+    		this.stroke = stroke;
+    		return this;
+    	}
+    	
+    	public ShapeBuilder fill(Integer fill){
+    		this.fill = fill;
+    		return this;
+    	}
+    	public ShapeBuilder width(Float size){
+        	this.width = size;
+        	return this;
+    	}
+    	public ShapeBuilder points(float[][] points){
+    		this.points = points;
+    		return this;
+    	}
+    	public Polygon build(){
+    		PolygonOptions poly = new PolygonOptions();
+    		for(float[] point : this.points)
+        		poly.add(new LatLng(point[0],point[1]));
+    		
+    		if(this.fill != null)
+        		poly.fillColor(this.fill);
+    		if(this.width != null)
+        		poly.strokeWidth(this.width);
+    		if(this.stroke != null)
+        		poly.strokeColor(this.stroke);
+    		
+    		return eventMap.addPolygon(poly);
+    	}
     	
     }
      
