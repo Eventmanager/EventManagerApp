1e144d20091e2b025d06c5bd4c92039905511327
Seph <bob.vankamp@gmail.com>
2014-05-30 20:16:59 +0200

    Started adding planning
    Nog niet erg uitgewerkt, maar het begin is er :D

--- src/com/project/eventmanagerapp/Activity_Planning.java
+++ src/com/project/eventmanagerapp/Activity_Planning.java
@@ -1,7 +1,14 @@
 package com.project.eventmanagerapp;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.util.AttributeSet;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.LinearLayout.LayoutParams;
+import android.widget.TextView;
 
 public class Activity_Planning extends Activity {
 	
@@ -9,5 +16,49 @@ public class Activity_Planning extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_planning);
+		
+	    LinearLayout mainlinear= (LinearLayout) findViewById(R.id.planninglayout);
+	    
+	    String[][] planninginfo = new String[][]{{"Begin van dit stuk","Nog wat van dit","En tot slot dit en dan eindigt het ongeveer"},{"Hier begint deze","Gaat hier weer verder","En uiteindelijk hier"},
+	    										{"Deze gaat hier van start", "Heeft wat meer dan de rest", "Kort", "Eindigt hier"}};
+	    
+	    
+		LinearLayout [] linear = new LinearLayout[planninginfo.length];
+
+		for(int i=0;i<linear.length;i++)
+		{
+			linear[i] = new LinearLayout(this);
+			LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
+					LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT);
+			mainlinear.addView(linear[i], params);
+			linear[i].setOrientation(LinearLayout.HORIZONTAL);
+		
+		
+			TextView [] txt =new TextView[planninginfo[i].length];
+			
+	
+			for(int j=0;j<txt.length;j++)
+			{
+				TextView divider = new TextView(Activity_Planning.this);
+				
+				LinearLayout.LayoutParams llp = new LinearLayout.LayoutParams(pxtodp(2), LayoutParams.MATCH_PARENT);
+			    llp.setMargins(pxtodp(5), 0, pxtodp(5), 0); // llp.setMargins(left, top, right, bottom);
+			    divider.setLayoutParams(llp);
+			    
+				divider.setBackgroundColor(Color.GRAY);
+				
+				txt[j]=new TextView(Activity_Planning.this);
+				txt[j].setText(planninginfo[i][j]);
+				txt[j].setLayoutParams(new LayoutParams(LayoutParams.WRAP_CONTENT,LayoutParams.WRAP_CONTENT));
+				
+				linear[i].addView(txt[j]);
+				if(j != txt.length-1)
+					linear[i].addView(divider);
+			}
+		}
+	}
+	
+	private int pxtodp(int px){
+		return (int)(getResources().getDisplayMetrics().density*px);
 	}
 }
