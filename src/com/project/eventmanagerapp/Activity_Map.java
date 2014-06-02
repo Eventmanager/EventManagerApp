2fffb1498118858e28164471d6624eeb4475104c
Seph <bob.vankamp@gmail.com>
2014-06-02 21:45:52 +0200

    Commented out example code for map
    Don't want to display that crap at our demo :D

--- src/com/project/eventmanagerapp/Activity_Map.java
+++ src/com/project/eventmanagerapp/Activity_Map.java
@@ -46,12 +46,12 @@ public class Activity_Map extends FragmentActivity {
     	eventMap.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(51.9167, 4.5000), 12.0f));
     	
     	//Example code for shapes. In this case a triangle near the marker.
-    	Polygon poly1 = new ShapeBuilder().points(new float[][]{{51.9167f,4.5000f},{51.9177f,4.5006f},{51.9157f,4.5090f}}).build();
+    	/*Polygon poly1 = new ShapeBuilder().points(new float[][]{{51.9167f,4.5000f},{51.9177f,4.5006f},{51.9157f,4.5090f}}).build();
     	Polygon poly2 = new ShapeBuilder().points(new float[][]{{51.9187f,4.5000f},{51.9197f,4.5006f},{51.9177f,4.5090f}})
-    			.width(5f).fill(Color.RED).stroke(Color.BLUE).build();
+    			.width(5f).fill(Color.RED).stroke(Color.BLUE).build();*/
     	
     	//Example code for an image. In this case near the marker.
-    	GroundOverlay image1 = new ImageBuilder().position(new LatLng(51.9167, 4.4980)).width(2000f).image("house").build();
+    	/*GroundOverlay image1 = new ImageBuilder().position(new LatLng(51.9167, 4.4980)).width(2000f).image("house").build();*/
     }
     
     
