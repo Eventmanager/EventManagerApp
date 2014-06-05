122043982219c2179cc66ecdba4bc36a379b6353
Seph <bob.vankamp@gmail.com>
2014-06-05 17:07:19 +0200

    You can now select and save things in the planning

--- src/com/project/eventmanagerapp/Activity_Planning.java
+++ src/com/project/eventmanagerapp/Activity_Planning.java
@@ -1,7 +1,15 @@
 package com.project.eventmanagerapp;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.text.TextUtils.TruncateAt;
@@ -16,6 +24,11 @@ import android.widget.LinearLayout.LayoutParams;
 import android.widget.TextView;
 
 public class Activity_Planning extends Activity {
+	
+	LinearLayout [] linear;
+	List<List> textList;
+	SharedPreferences sharedPrefs;
+	JSONArray savedEvents = null;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -32,7 +45,8 @@ public class Activity_Planning extends Activity {
 	    										{new Event("Sonata Arctica",0,1f), new Event("Justin Bieber",1f,1.5f), new Event("Pharrell Williams",1.5f,2.4f), new Event("Afrojack",2.4f,3.5f)}};
 	    
 	    
-		LinearLayout [] linear = new LinearLayout[planninginfo.length];
+		linear = new LinearLayout[planninginfo.length];
+		textList = new ArrayList<List>();
 
 		for(int i=0;i<linear.length;i++)
 		{
@@ -41,6 +55,8 @@ public class Activity_Planning extends Activity {
 					LayoutParams.MATCH_PARENT, pxtodp(60));
 			mainlinear.addView(linear[i], params);
 			linear[i].setOrientation(LinearLayout.HORIZONTAL);
+			
+			textList.add(i,new ArrayList<TextView>());
 		
 		
 			TextView [] txt =new TextView[planninginfo[i].length];
@@ -64,7 +80,9 @@ public class Activity_Planning extends Activity {
 				txt[j].setSingleLine(true);
 				txt[j].setEllipsize(TruncateAt.END);
 				txt[j].setTextSize(20);
-				txt[j].setOnClickListener(new EventClickListener(planninginfo[i][j].getName()));
+				txt[j].setOnClickListener(new EventClickListener(i,j,planninginfo[i][j].getName()));
+				
+				textList.get(i).add(txt[j]);
 				
 				linear[i].addView(txt[j]);
 				if(j != txt.length-1)
@@ -100,9 +118,39 @@ public class Activity_Planning extends Activity {
 		
 	}
 	
-	
-	public void onClick(View v,String s){
+	@Override
+	protected void onResume(){
+		super.onResume();
+		sharedPrefs = getSharedPreferences("EventPrefs",MODE_PRIVATE);
+		if(sharedPrefs.contains("savedevents"))
+		{
+			try {
+				savedEvents = new JSONArray(sharedPrefs.getString("savedevents", null));
+			} catch (JSONException e) {
+				e.printStackTrace();
+			}
+		}
+		else
+			savedEvents = new JSONArray();
 		
+		for(List l : textList){
+			for(Object v : l){
+				((TextView)v).setBackgroundColor(Color.WHITE);
+			}
+		}
+		
+		for(int i=0;i<savedEvents.length();i++)
+		{
+			try {
+				int podium = ((JSONObject) savedEvents.get(i)).getInt("podium");
+				int event = ((JSONObject) savedEvents.get(i)).getInt("event");
+				((TextView) textList.get(podium).get(event)).setBackgroundColor(Color.RED);
+			} catch (JSONException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+		}
 	}
 	
 	private int pxtodp(int px){
@@ -150,16 +198,22 @@ public class Activity_Planning extends Activity {
 	public class EventClickListener implements OnClickListener
 	   {
 
+	     int podium;
+	     int event;
 	     String name;
-	     public EventClickListener(String eventname) {
-	          this.name = eventname;
+	     public EventClickListener(int p,int e,String n) {
+	          this.podium = p;
+	          this.event = e;
+	          this.name = n;
 	     }
 
 	     @Override
 	     public void onClick(View v)
 	     {
 	    	 Intent newpage = new Intent("com.project.eventmanagerapp.Activity_Planninginfo");		
-	    	 newpage.putExtra("eventname", this.name);
+	    	 newpage.putExtra("podium_id", this.podium);
+	    	 newpage.putExtra("event_id", this.event);
+	    	 newpage.putExtra("event_name",this.name);
 	    	 startActivity(newpage);
 	     }
 
--- src/com/project/eventmanagerapp/Activity_Planninginfo.java
+++ src/com/project/eventmanagerapp/Activity_Planninginfo.java
@@ -1,32 +1,141 @@
 package com.project.eventmanagerapp;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.text.TextUtils.TruncateAt;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.LinearLayout.LayoutParams;
 import android.widget.TextView;
 
 public class Activity_Planninginfo extends Activity {
 	
+	int current_podium;
+	int current_event;
+	JSONArray savedEvents = null;
+	List<JSONObject> savedEventsList = new ArrayList<JSONObject>();
+	SharedPreferences sharedPrefs;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_planninginfo);
 		Intent myIntent = getIntent();
+		current_podium = myIntent.getIntExtra("podium_id",0);
+		current_event = myIntent.getIntExtra("event_id",0);
 
 		TextView txt=new TextView(Activity_Planninginfo.this);
-		txt.setText(myIntent.getStringExtra("eventname"));
+		txt.setText(myIntent.getStringExtra("event_name"));
 		txt.setLayoutParams(new LayoutParams(LayoutParams.WRAP_CONTENT,LayoutParams.WRAP_CONTENT));
 		LinearLayout mainlinear= (LinearLayout) findViewById(R.id.eventinfo_layout);
 		mainlinear.addView(txt);
 		
+		sharedPrefs = getSharedPreferences("EventPrefs",MODE_PRIVATE);
+		if(sharedPrefs.contains("savedevents"))
+		{
+			try {
+				savedEvents = new JSONArray(sharedPrefs.getString("savedevents", null));
+			} catch (JSONException e) {
+				e.printStackTrace();
+			}
+		}
+		else
+			savedEvents = new JSONArray();
+		
+		savedEventsList = jsonToList(savedEvents);
+
+		Log.d("String",savedEvents.toString());
+		
+		Button bt = new Button(this);
+		bt.setText("Add event");
+		bt.setLayoutParams(new LayoutParams(LayoutParams.WRAP_CONTENT,LayoutParams.WRAP_CONTENT));
+		bt.setOnClickListener(new View.OnClickListener() {			
+			@Override
+			public void onClick(View v) {
+				saveEvent(current_podium,current_event);
+				savedEvents = listToJson(savedEventsList);
+				Editor editor = sharedPrefs.edit();
+				editor.putString("savedevents",savedEvents.toString());
+				editor.commit();
+			}
+		});
+		
+		Button bt2 = new Button(this);
+		bt2.setText("Remove event");
+		bt2.setLayoutParams(new LayoutParams(LayoutParams.WRAP_CONTENT,LayoutParams.WRAP_CONTENT));
+		bt2.setOnClickListener(new View.OnClickListener() {			
+			@Override
+			public void onClick(View v) {
+				removeEvent(current_podium,current_event);
+				savedEvents = listToJson(savedEventsList);
+				Editor editor = sharedPrefs.edit();
+				editor.putString("savedevents",savedEvents.toString());
+				editor.commit();
+			}
+		});
+		
+		mainlinear.addView(bt);
+		mainlinear.addView(bt2);
+		
+		
+	}
+	
+	private void saveEvent(int podium, int event){
+		try{
+			for(JSONObject i : savedEventsList)
+				if(i.getInt("podium") == podium && i.getInt("event") == event)
+					return;
+			savedEventsList.add(new JSONObject().put("podium", podium).put("event", event));
+			} catch (JSONException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+	}
+	
+	private void removeEvent(int podium,int event){
+		try {
+			for(int i=0;i<savedEventsList.size();i++)
+				if(savedEventsList.get(i).getInt("podium") == podium && savedEventsList.get(i).getInt("event") == event)
+			      savedEventsList.remove(i);
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	private List jsonToList(JSONArray arr){
+		List<JSONObject> tmp = new ArrayList<JSONObject>();
+		for(int i=0;i<arr.length();i++)
+			try {
+				tmp.add((JSONObject) arr.get(i));
+			} catch (JSONException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		return tmp;
+	}
+	
+	private JSONArray listToJson(List<JSONObject> l){
+		JSONArray tmp = new JSONArray();
+		for(JSONObject i: l)
+			tmp.put(i);
+		return tmp;
 	}
 	
 }
